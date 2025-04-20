87e5bc612839dd38e0744a15c05e2072262f16c7
briodan <41130293+briodan@users.noreply.github.com>
2025-04-20 01:35:44 +0000

    v0.2

--- app.py
+++ app.py
@@ -29,3 +29,6 @@ def get_dirty_line():
         return jsonify({"line": line})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+        
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
